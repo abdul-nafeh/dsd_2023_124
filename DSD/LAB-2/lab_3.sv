f22293decf2110025a45b43da5c1bf5e42230d63
abdul-nafeh <abdulnafeh147@gmail.com>
2025-02-03 22:52:02 +0500

    Change file

---
+++ DSD/LAB-2/lab_3.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/03/2025 05:45:01 PM
+// Design Name: 
+// Module Name: lab_3
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module lab_3(
+
+    );
+endmodule
