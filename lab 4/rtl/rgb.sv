ed49a7d8976059c35978bf6c415d7e725e9878fa
abdul-nafeh <abdulnafeh147@gmail.com>
2025-02-17 22:26:08 +0500

    files added

---
+++ lab 4/rtl/rgb.sv
@@ -0,0 +1,15 @@
+`timescale 1ns / 1ps
+
+module rgb(
+ input logic [1:0] a,b,
+ output logic red,green,blue 
+);
+
+ assign red = (a[1] & ~b[0]) | (a[1] & ~b[1]) | (a[0] & a[1]) | (~b[0] & ~b[1]) | (a[0] & ~b[1]);
+ 
+ assign blue = (~a[0] & b[0]) | (a[1] & ~b[1]) | (a[0] & ~b[0]) | (~a[1] & b[1]);
+ 
+ assign green = (~a[0] & ~a[1]) | (~a[0] & b[1]) | (b[0] & b[1]) | (~a[1] & b[1]) | (~a[1] & b[0]);
+
+endmodule
+
